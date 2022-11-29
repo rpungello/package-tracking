5c53c44a3d83c5e3cb76728a1712f18b484f4550
Rob Pungello <rob.pungello@gmail.com>
2022-11-29 10:44:54 -0500

    Add test with multiple carriers

---
+++ tests/MultiTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use Ramsey\Collection\Collection;
+use Rpungello\PackageTracking\Carriers\FedEx;
+use Rpungello\PackageTracking\Carriers\UPS;
+use Rpungello\PackageTracking\Package;
+use Rpungello\PackageTracking\PackageTracking;
+
+it('can extract ups and fedex tracking numbers', function () {
+    $instance = new PackageTracking();
+    $ups = new UPS();
+    $fedex = new FedEx();
+
+    $text = "First line has a tracking number 1Z12345E0305271640 and some other text\nSecond line has 98000000000 0000 0000";
+    $expected = new Collection(Package::class, [new Package($ups, '1Z12345E0305271640'), new Package($fedex, '98000000000 0000 0000')]);
+
+    $results = $instance->parsePackages($text);
+    expect($results)->toHaveCount($expected->count());
+
+    for ($i = 0; $i < $expected->count(); $i++) {
+        expect($results[$i]->carrier->getName())->toBe($expected[$i]->carrier->getName());
+        expect($results[$i]->trackingNumber)->toBe($expected[$i]->trackingNumber);
+    }
+});
